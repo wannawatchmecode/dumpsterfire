877d5e31598277435371880061049f41756d1996
wannawatchmecode <wannawatchmecode@gmail.com>
2024-11-03 15:45:35 -0500

    Add ocean integration

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express({});
 const netstat = require('node-netstat');
+const io = require('socket.io-client');
+
+const OCEAN_HOST = 'http://localhost:7476/dumpsterfire/admin';
+
+const OCEAN_AUTH_TOKEN = '1234-4321-9876-678';
+
+const socket = io(OCEAN_HOST, {
+	auth: {
+		token: OCEAN_AUTH_TOKEN
+	}
+});
+
+let socketio;
+
+
+			socket.on("connect", () => {
+  				console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+				socketio = socket.io;
+			});
+
+			socket.on("disconnect", () => {
+  				console.log(socket.id); // undefined
+				socketio = undefined;
+			});
+
+
 
 const parser = netstat.parserFactories.linux({});
 
@@ -11,7 +37,7 @@ app.use(bodyParser.json());
 app.post('/health/report', async (req, res) => {
 	console.log(req.body);
 	console.log(`Length: ${JSON.stringify(req.body).length}`);
-
+	socket.emit('healthEvent', req.body);
 	res.status = 200;
 	res.json({
 		Message: "received"
